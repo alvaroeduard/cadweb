4c638203ef9196ac7447afd6fda9875a8b7c3d8e
alvaro <catzs.2022112linf0030@aluno.ifpi.edu.br>
2025-02-10 18:39:29 -0300

    10/02/2025 18:39

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('categoria', views.categoria, name="categoria"),
